5dc0d7f9d12661ec1c584cf7b87ea38af534e7e8
ctam91 <ctam91@gmail.com>
2017-10-25 00:22:31 -0700

    add Weather data

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.tammy.weatherproject.Models.QueryUtils;
+import org.tammy.weatherproject.Models.Weather;
 
 
 @Controller
@@ -15,6 +16,9 @@ public class HomeController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model){
+        Weather theWeather = QueryUtils.fetchWeatherData("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1");
+
+        model.addAttribute(theWeather);
         return "index";
     }
 }
