31a7477f0c7f5a75a96b6d8303e1632e150a6a02
ctam91 <ctam91@gmail.com>
2017-10-25 11:29:02 -0700

    add location search

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.tammy.weatherproject.Models.QueryUtils;
 import org.tammy.weatherproject.Models.Weather;
 
@@ -28,7 +29,12 @@ public class HomeController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String index(){
-        return null;
+    public String index(Model model, @RequestParam String city){
+        String image = "http://api.wunderground.com/api/cb5d7b2fbd91dacc/animatedradar/q/"+ "WA" + "/" + city + ".gif?newmaps=1";
+        Weather theWeather = QueryUtils.fetchWeatherData("http://api.wunderground.com/api/cb5d7b2fbd91dacc/conditions/q/"+"WA" +"/"+ city +".json");
+
+        model.addAttribute("imageURL", image);
+        model.addAttribute(theWeather);
+        return "jumbotron";
     }
 }
