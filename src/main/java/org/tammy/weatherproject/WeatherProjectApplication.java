2da697633932857d790456601d96606be9abddb0
ctam91 <ctam91@gmail.com>
2017-10-24 20:33:12 -0700

    update templates

--- src/main/java/org/tammy/weatherproject/WeatherProjectApplication.java
+++ src/main/java/org/tammy/weatherproject/WeatherProjectApplication.java
@@ -1,4 +1,4 @@
-package org.tammy.weatherproject.Models;
+package org.tammy.weatherproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
