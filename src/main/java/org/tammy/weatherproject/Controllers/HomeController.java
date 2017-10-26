9520d14c585fcd39912550dc7e490919e0dd32b2
ctam91 <ctam91@gmail.com>
2017-10-26 00:00:33 -0700

    refactor code

--- src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
+++ src/main/java/org/tammy/weatherproject/Controllers/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
         model.addAttribute("forecasts", forecasts);
         model.addAttribute("imageURL", image);
         model.addAttribute("weather",theWeather);
-        return "jumbotron";
+        return "index";
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
@@ -49,7 +49,7 @@ public class HomeController {
         model.addAttribute("imageURL", image);
         model.addAttribute("weather",theWeather);
 
-        return "jumbotron";
+        return "index";
     }
 
 }
